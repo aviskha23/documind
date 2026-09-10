954506226411ede0f81ff26a552df21fa2803657
aviskha23 <aviskha5643@gmail.com>
2026-09-10 22:21:20 +0530

    simplify Clerk middleware

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  frontendApiProxy: {
-    enabled: (url) => process.env.NODE_ENV === "production",
-  },
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|png|jpg|jpeg|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
-    "/__clerk/(.*)",
   ],
 };
